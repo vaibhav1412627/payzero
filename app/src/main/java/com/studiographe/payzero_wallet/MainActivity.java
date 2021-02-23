04dd2f58701abe693f03a8f30bcb3dcc45df2e11
Dell <harshadaphalle@gmail.com>
2021-02-23 12:24:22 +0530

    custom toolbar

--- app/src/main/java/com/studiographe/payzero_wallet/MainActivity.java
+++ app/src/main/java/com/studiographe/payzero_wallet/MainActivity.java
@@ -12,13 +12,20 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
